f8df6c984f6986199092a051667e5fd0c2f0acf9
tomtargosz <tom.targosz@yahoo.com>
2019-03-08 16:12:33 -0600

    Ran prettify. Add !drop command, add better error scenarios for args

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
+// @flow
 const Discord = require("discord.js");
-const MongoClient = require('mongodb').MongoClient;
-const _ = require('lodash');
-const request = require('request');
-const moment = require('moment');
+const MongoClient = require("mongodb").MongoClient;
+const _ = require("lodash");
+const request = require("request");
+const moment = require("moment");
 
 const bot = new Discord.Client();
-const getItemID = require('./utils.js');
+const { getItemID, calculateUniqueProbability } = require("./utils.js");
 const config = require("./config.json");
 
 const ITEM_DATA = require("./itemData.json");
 
-const DB_URI = `mongodb+srv://${config.db_user}:${config.db_password}@${config.db_url}`;
+const DB_URI = `mongodb+srv://${config.db_user}:${config.db_password}@${
+  config.db_url
+}`;
 const ROTATIONS_IMAGE_URL = "https://i.redd.it/hdfsf45xwzqy.png";
-const OSRS_GE_BASE_URL = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=";
-const HELP_MESSAGE = "This bot supports the following commands:\n- !rotation: displays the different possible raid rotations\n- !item: displays the drop rate and current price of any raids items";
+const OSRS_GE_BASE_URL =
+  "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=";
+const HELP_MESSAGE =
+  "This bot supports the following commands:\n" +
+  "- `!help`: provides an overview of the bots functionality\n" +
+  "- `!rotation`: displays the different possible raid rotations\n" +
+  "- `!item`: displays the drop rate and current price of any raids items\n" +
+  "- `!drop`: calculates the % chance of receiving a unique drop based on the amount of points accumulated in a raid";
+const COMMANDS = {
+  HELP: "help",
+  ROTATION: "rotation",
+  ITEM: "item",
+  DROP: "drop"
+};
 
 bot.on("ready", () => {
-  console.log('Bot has started successfully!');
-  console.log('Users interacting with bot:', bot.users.size)
-  console.log('Channels with bot:', bot.channels.size);
-  console.log('Guilds with bot:', bot.guilds.size);
+  console.log("Bot has started successfully!");
 
   bot.user.setPresence({
-    status: 'Online',
+    status: "Online",
     game: {
-      name: 'Old School RuneScape',
-      type: 'LISTENING',
-    },
+      name: "Old School RuneScape",
+      type: "LISTENING"
+    }
   });
 
   updateDBItems();
@@ -40,29 +52,47 @@ bot.on("message", async message => {
     return;
   }
 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const args = message.content
+    .slice(config.prefix.length)
+    .trim()
+    .split(/ +/g);
   const command = args.shift().toLowerCase();
+  const { HELP, ROTATION, DROP, ITEM } = COMMANDS;
+  const commandsWithArgs = [DROP, ITEM];
 
-  switch(command) {
-    case "help":
+  if (!COMMANDS[command.toUpperCase()]) {
+    message.channel.send(
+      "ERROR: Command `" +
+        command +
+        "` does not exist! Use `!help` for an overview of functionality."
+    );
+  }
+  if (commandsWithArgs.indexOf(command) !== -1 && _.isEmpty(args)) {
+    message.channel.send(
+      "ERROR: You must supply an arguement for this command! Use `!help` for an overview of functionality."
+    );
+    return;
+  }
+
+  switch (command) {
+    case HELP:
       message.channel.send(HELP_MESSAGE);
       break;
-    case "rotation":
+    case ROTATION:
       message.channel.send(ROTATIONS_IMAGE_URL);
       break;
-    case "item":
-      if (!_.isEmpty(args)) {
-        const itemID = getItemID(args.join(' '));
-        if (!!itemID) {
-          const retVal = await getSingleItem(itemID, buildSingleItemTable);
-          if (!!retVal) {
-            message.channel.send(buildSingleItemTable(retVal));
-          }
-        } else {
-          message.channel.send("ERROR: Item not found!");
+    case DROP:
+      message.channel.send(calculateUniqueProbability(args.join(" ")));
+      break;
+    case ITEM:
+      const itemID = getItemID(args.join(" "));
+      if (!!itemID) {
+        const retVal = await getSingleItem(itemID);
+        if (!!retVal) {
+          message.channel.send(buildSingleItemTable(retVal));
         }
       } else {
-        message.channel.send("ERROR: Supply an item to look up!");
+        message.channel.send("ERROR: Item not found!");
       }
       break;
     default:
@@ -75,20 +105,23 @@ bot.login(config.token);
 const getExchangePrice = (itemID, callback) => {
   const url = OSRS_GE_BASE_URL + itemID;
 
-  request({
-    url: url,
-    json: true
-  }, (error, response) => {
-    if (error || response.statusCode !== 200) {
-      return callback(error || {statusCode: response.statusCode});
+  request(
+    {
+      url: url,
+      json: true
+    },
+    (error, response) => {
+      if (error || response.statusCode !== 200) {
+        return callback(error || { statusCode: response.statusCode });
+      }
+      callback(itemID, response);
     }
-    callback(itemID, response);
-  });
-}
+  );
+};
 
 // Updates a single item by id
 const updateDBItem = (itemID, response) => {
-  const currentPrice = _.get(response, 'body.item.current.price');
+  const currentPrice = _.get(response, "body.item.current.price");
   if (!currentPrice) {
     console.log("Error retrieving response for item:", itemID);
   }
@@ -98,7 +131,9 @@ const updateDBItem = (itemID, response) => {
 
     const items = client.db(config.db_name).collection(config.db_collection);
     const itemQuery = { id: itemID };
-    const updateQuery = { $set: { value: currentPrice, lastUpdated: moment() } };
+    const updateQuery = {
+      $set: { value: currentPrice, lastUpdated: moment() }
+    };
 
     items.updateOne(itemQuery, updateQuery, (err, res) => {
       if (err) {
@@ -108,7 +143,7 @@ const updateDBItem = (itemID, response) => {
       client.close();
     });
   });
-}
+};
 
 // Updates all items in the DB
 const updateDBItems = () => {
@@ -117,31 +152,35 @@ const updateDBItems = () => {
   _.forEach(ITEM_DATA.items, item => {
     getExchangePrice(item.id, updateDBItem);
   });
-  
+
   console.log("Items updated!");
-}
+};
 
 async function getSingleItem(itemID) {
   const client = await MongoClient.connect(DB_URI);
-  const result = await client.db(config.db_name).collection(config.db_collection).find({ id: itemID }).toArray();
+  const result = await client
+    .db(config.db_name)
+    .collection(config.db_collection)
+    .find({ id: itemID })
+    .toArray();
 
   return result;
 }
 
-const buildSingleItemTable = (response) => {
+const buildSingleItemTable = response => {
   if (!response) {
-    console.log('null response');
+    console.log("null response");
     return null;
   }
-  const itemName = _.get(response[0], 'itemName', 'N/A');
-  const value = _.get(response[0], 'value', 'N/A');
-  const probability = _.get(response[0], 'probability', 'N/A');
+  const itemName = _.get(response[0], "itemName", "N/A");
+  const value = _.get(response[0], "value", "N/A");
+  const probability = _.get(response[0], "probability", "N/A");
 
   let message = new Discord.RichEmbed();
   message
     .addField("Item", itemName, true)
-    .addField("Price",  value, true)
-    .addField("Drop Rate",  probability, true);
+    .addField("Price", value, true)
+    .addField("Drop Rate", probability, true);
 
   return message;
-}
+};
